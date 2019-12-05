87a5813acf92eac63b0e4aca676aee872ad421fb
张江浩 <zhangjianghao@zhuojianchina.com>
2019-12-05 10:52:31 +0800

    初始化

---
+++ src/test/java/com/zjh/rocketmqtest/RocketmqTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjh.rocketmqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketmqTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
